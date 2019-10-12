3f398e641155cbe6b753c4d36a4f9ef4242a01fa
sphynx-henryay <pyc05079@gmail.com>
2019-10-12 10:06:55 +0000

    Unify ComposeField output interface

--- fields/compose/fields.py
+++ fields/compose/fields.py
@@ -1,6 +1,14 @@
 from ..base import Fields
 
-class ComposeField( Fields.Field ): pass
+class ComposeField( Fields.Field ):
+
+	_composed = None
+
+	def get_composed( self, *args, **kwargs ):
+		pass
+
+	def __call__( self, *args, **kwargs ):
+		return self.get_composed( *args, **kwargs )
 
 class ComposeTypes( Fields.Types ): pass
 
