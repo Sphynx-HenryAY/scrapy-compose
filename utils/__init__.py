7d42527d0d1fad6fc17f4214ee059963a414fd89
sphynx-henryay <pyc05079@gmail.com>
2019-10-13 09:15:16 +0000

    Added classproperty decorator

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,10 @@ import random, string
 
 def genuid( length = 22 ):
 	return ''.join( random.choice( string.ascii_letters + string.digits ) for x in range( length ) )
+
+class classproperty:
+	def __init__( self, f ):
+		self.f = classmethod( f )
+
+	def __get__( self, *args ):
+		return self.f.__get__( *args )()
