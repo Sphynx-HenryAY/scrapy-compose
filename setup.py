3e38011114daa400c3ed928664f39331810d5bd3
sphynx-henryay <pyc05079@gmail.com>
2019-10-18 03:39:32 +0000

    Updated setup.py info

--- setup.py
+++ setup.py
@@ -3,16 +3,22 @@ from setuptools import setup, find_packages
 setup(
 	name = "scrapy-compose",
 	version = "0.8",
-	packages = find_packages(),
 	author = "Henry AY",
 	author_email = "pyc05079@gmail.com",
 	url = "https://github.com/Sphynx-HenryAY/scrapy-compose.git",
-	entry_points = {
-		"console_scripts": [ 'scrapy-compose = scrapy_compose.cmdline:main' ]
-	},
-	python_requires = '>=3',
+	packages = find_packages(),
+	classifiers = [
+		"Operating System :: OS Independent",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3.5",
+		"Framework :: Scrapy",
+	],
+	python_requires = '>=3.5',
 	install_requires = [
 		"Scrapy",
 		"PyYaml"
-	]
+	],
+	entry_points = {
+		"console_scripts": [ 'scrapy-compose = scrapy_compose.cmdline:main' ]
+	},
 )
