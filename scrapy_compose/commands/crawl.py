6855b47c225de17f6040dbf16ab1c0160f6fd4e6
Henry Au-Yeung <henry.hn.au-yeung@pccw.com>
2019-10-21 18:10:01 +0800

    Crawl spiders with name matched with provided pattern

--- scrapy_compose/commands/crawl.py
+++ scrapy_compose/commands/crawl.py
@@ -7,3 +7,18 @@ class Command( BaseCommand ):
 
 	def short_desc( self ):
 		return "Run spiders with name matched with pattern."
+
+	def run( self, args, opts ):
+		from scrapy_compose.utils.command import spider_filter
+
+		s_list = spider_filter(
+			self.crawler_process.spider_loader.list(),
+			args.pop( 0 )
+		)
+
+		for s_name in s_list:
+			self.crawler_process.crawl( s_name, **opts.spargs )
+
+		self.crawler_process.start()
+		if self.crawler_process.bootstrap_failed:
+			self.exitcode = 1
