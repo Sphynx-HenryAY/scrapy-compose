5c7b1f465c99dd9ecafc9eed9c4a120632a91153
Henry Au-Yeung <henry.hn.au-yeung@pccw.com>
2019-10-21 18:42:57 +0800

    Show length of spider list found with -c or --count

--- scrapy_compose/commands/list.py
+++ scrapy_compose/commands/list.py
@@ -8,12 +8,20 @@ class Command( BaseCommand ):
 	def short_desc( self ):
 		return "List available spiders matched with pattern if provided."
 
+	def add_options( self, parser ):
+		super( Command, self ).add_options( parser )
+		parser.add_option( "-c", "--count", dest = "count", action = "store_true", default = False )
+
 	def run( self, args, opts ):
 
 		s_list = self.crawler_process.spider_loader.list()
 		if args:
 			from scrapy_compose.utils.command import spider_filter
 			s_list = spider_filter( s_list, args[0] )
+
+		if opts.count:
+			print( len( s_list ), "spiders matched." )
+			return
 
 		for s in sorted( s_list ):
 			print( s )
